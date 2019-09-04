4c15e9c1386ffe902b8b13492b9864380dd22f12
Dmitry Dubrovin <dmmitriyd63@gmail.com>
2019-09-04 19:37:14 +0500

    Create repository, service(interface and base implementation) and controller. Adding annotation to model

---
+++ src/main/java/com/space/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.space.controller;
+
+import com.space.model.Ship;
+import com.space.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/rest")
+public class MainController {
+
+}
---
+++ src/main/java/com/space/service/ShipServiceImpl.java
@@ -0,0 +1,52 @@
+package com.space.service;
+
+import com.space.model.Ship;
+import com.space.repository.ShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ShipServiceImpl implements ShipService{
+    @Autowired
+    private ShipRepository shipRepository;
+
+
+    @Override
+    public List<Ship> getAllShips() {
+
+        return null;
+    }
+
+    @Override
+    public Ship addShip(Ship ship) {
+        return null;
+    }
+
+    @Override
+    public Ship editShip(Ship ship) {
+        return null;
+    }
+
+    @Override
+    public void deleteShip(Long id) {
+
+    }
+
+    @Override
+    public Ship getShipById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Ship> getSelectedShips() {
+        return null;
+    }
+
+    @Override
+    public int countSelectedShips(List<Ship> selected) {
+        return 0;
+    }
+}
